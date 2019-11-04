8ddefdf4f5f066da02fce4fe73ae9a0374c786b5
tak <dhdyddus3@naver.com>
2019-11-04 17:06:59 +0900

    FreeMode 작업

--- app/src/main/java/com/tagsoft/mazegame/FreeModeActivity.java
+++ app/src/main/java/com/tagsoft/mazegame/FreeModeActivity.java
@@ -1,7 +1,11 @@
 package com.tagsoft.mazegame;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -11,6 +15,7 @@ public class FreeModeActivity extends AppCompatActivity {
     GridView gridView;
     MyAdapter adapter;
     ArrayList<ClearData> datas = new ArrayList<>();
+    int stage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +30,34 @@ public class FreeModeActivity extends AppCompatActivity {
             datas.add(new ClearData(i+1, 0, "00", "00"));
         }
 
-
-
         adapter = new MyAdapter(getLayoutInflater(), datas);
         gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                stage = position+1;
+                Intent intent = new Intent(FreeModeActivity.this, StageActivity.class);
+                intent.putExtra("stage", stage);
+                startActivityForResult(intent, 20);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 20:
+                if(resultCode == RESULT_OK){
+                    int rating = data.getIntExtra("rating", 0);
+                    String minute = data.getStringExtra("minute");
+                    String second = data.getStringExtra("second");
+                    datas.get(stage=1).setStarsNumber(rating);
+                    datas.get(stage=1).setMinute(minute);
+                    datas.get(stage=1).setSecond(second);
+                }
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/tagsoft/mazegame/StageActivity.java
@@ -0,0 +1,75 @@
+package com.tagsoft.mazegame;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
+public class StageActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    ScrollView verticalScroll;
+    HorizontalScrollView horizontalScroll;
+    GameView gameView;
+    JoystickView joystick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage);
+
+        intent = getIntent();
+        int stage = intent.getIntExtra("stage", 1);
+
+        getSupportActionBar().setTitle("stage "+stage);
+
+        verticalScroll = findViewById(R.id.vertical_scroll);
+        horizontalScroll = findViewById(R.id.horizontal_scroll);
+        gameView = findViewById(R.id.gameview);
+        joystick = findViewById(R.id.joystick);
+        joystick.setBackgroundColor(R.drawable.joystick_background);
+        joystick.setOnMoveListener(moveListener);
+    }
+
+    JoystickView.OnMoveListener moveListener = new JoystickView.OnMoveListener() {
+        @Override
+        public void onMove(int angle, int strength) {
+            if (strength > 80) {
+                if (angle <= 45 || angle > 315) {
+                    gameView.movePlayer(GameView.Direction.RIGHT);
+                } else if (angle <= 135 && angle > 45) {
+                    gameView.movePlayer(GameView.Direction.UP);
+                } else if (angle <= 225 && angle > 135) {
+                    gameView.movePlayer(GameView.Direction.LEFT);
+                } else if (angle <= 315 && angle > 225) {
+                    gameView.movePlayer(GameView.Direction.DOWN);
+                }
+            }else if(strength>0 && strength<=80){
+                if (angle <= 45 || angle > 315) {
+                    gameView.movePlayer(GameView.Direction.RIGHT);
+                } else if (angle <= 135 && angle > 45) {
+                    gameView.movePlayer(GameView.Direction.UP);
+                } else if (angle <= 225 && angle > 135) {
+                    gameView.movePlayer(GameView.Direction.LEFT);
+                } else if (angle <= 315 && angle > 225) {
+                    gameView.movePlayer(GameView.Direction.DOWN);
+                }
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            verticalScroll.scrollTo((gameView.player.col - 5) * 100, (gameView.player.row - 5) * 100);
+            horizontalScroll.scrollTo((gameView.player.col - 5) * 100, (gameView.player.row - 5) * 100);
+
+            return;
+
+        }
+    };
+
+}
